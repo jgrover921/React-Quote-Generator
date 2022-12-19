35f51122dbbe5b65c8eaedf0713b086a83ca4be1
JessGrover <jgrover0921@gmail.com>
2022-12-19 14:16:06 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quote from './Components/Quotes'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<Quote />
+    </div>
+  );
+}
+
+export default App;
